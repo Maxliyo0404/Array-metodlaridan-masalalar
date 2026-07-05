fc4aa8afa98a0bdd9f1a539d2e868a2bfaca3c2b
Mahliyo <gasandigital13@gmail.com>
2026-07-05 19:43:36 +0500

    pop push metodlari

--- main.js
+++ main.js
@@ -11,10 +11,10 @@
 // console.log(users);
 // console.log(user);
 
-const numbers = [1, 2, 3, 4, 5, 6];
-// numbers.reverse();
-// console.log(numbers);
-//  numbers [0] ='10';
-numbers.push(40);
- console.log(numbers);
+// const numbers = [1, 2, 3, 4, 5, 6];
+// // numbers.reverse();
+// // console.log(numbers);
+// //  numbers [0] ='10';
+// numbers.push(40);
+//  console.log(numbers);
  

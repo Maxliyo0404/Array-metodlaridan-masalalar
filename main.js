3902a51feb3fb0d9814f180cb5c6b13ff4efee4c
Mahliyo <gasandigital13@gmail.com>
2026-07-05 19:13:20 +0500

    Array metodlari masalalar star

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const fruits = ["olma", "behi", "nok", "uzum"];
-// console.log( "Uzunligi", fruits.length);
-for(let i = 0; i < fruits.length ; i++ ){
-    console.log(fruits);
+// const fruits = ["olma", "behi", "nok", "uzum"];
+// // console.log( "Uzunligi", fruits.length);
+// for(let i = 0; i < fruits.length ; i++ ){
+//     console.log(fruits);
     
-}
+// }

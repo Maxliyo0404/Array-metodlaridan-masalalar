2891be692037e991d0ec48407098f9ec20350468
Mahliyo <gasandigital13@gmail.com>
2026-07-05 21:44:11 +0500

    pop push metodlari

--- main.js
+++ main.js
@@ -19,4 +19,4 @@
 //  console.log(numbers);
 //  console.log(typeof numbers);
  const users = [10, 20, 30, 40, 50,90];
- const user = users.filter((i => i))
+ const user = users.filter((i => i % 2 === 0))

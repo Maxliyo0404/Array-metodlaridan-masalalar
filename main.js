b35c7b2aa693ee24468aa780bbeef8f6a4498c8e
Mahliyo <gasandigital13@gmail.com>
2026-07-05 21:49:50 +0500

    metodlar

--- main.js
+++ main.js
@@ -19,8 +19,8 @@
 //  console.log(numbers);
 //  console.log(typeof numbers);
  const users = [10, 20, 30, 40, 50,90];
- const user = users.filter((i => i % 2 === 0){
-    
-    
+ const user = users.filter((i) => { return
+
  })
+
 console.log(user);

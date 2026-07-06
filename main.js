054dfd137c5cd53b3077155fa100ee37b1aa0477
Mahliyo <gasandigital13@gmail.com>
2026-07-06 22:49:26 +0500

    metodlardan masalalar to'plami

--- main.js
+++ main.js
@@ -24,6 +24,34 @@
 //  )
 
 // console.log(user);
-const num = [1, 5, 6, 9, 0, 3, 7];
-const numbers = num.splice("1", "7")
-console.log(  numbers);
+// const raqam = (raqam , index, array)=>{
+//     console.log(index);
+//     console.log(array);
+    
+    
+//     return raqam > 4
+// }
+// const num = [1, 5, 2, 4, 0, 6, 7];
+// const raqam = num.reduce((i, el)=> i + el, 0)
+// // const raqam = num.sort()
+// // const raqam  = num.findIndex((el)=> el > 6)
+// // const raqam = num.find((el)=>el > 4)
+// // const raqam = num.map((el)=> el * 2)
+// // const raqam = num.filter((el)=>el > 4);
+// console.log(raqam);
+
+
+// num.forEach((i, el)=>{
+//     if(el === 2)
+//     console.log(i);
+    
+// })
+// const numbers = num.find(raqam)
+// console.log(  numbers);
+// console.log(num);
+
+// const user = "SALOM-ANVAR";
+// const arr = user.split("-");
+// console.log(arr);
+
+con

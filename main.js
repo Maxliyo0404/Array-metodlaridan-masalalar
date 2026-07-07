c5abf309e1931978a491d04d0a259c0c1315a355
Mahliyo <gasandigital13@gmail.com>
2026-07-07 21:18:57 +0500

    split metodlar

--- main.js
+++ main.js
@@ -76,7 +76,7 @@
   //  console.log(b);
   //  console.log(a);
       let son = [2, 3, 8, 10, 6, 5];
-      let num = son.filter((el)=> el > 3)
+      let num = son.sort((a, b)=> b-a);
       // let num = son.find((el)=> el >= 3);
       console.log(num);
       

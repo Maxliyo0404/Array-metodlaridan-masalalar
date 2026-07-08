d260542340c96faf5c39bdd5407f47f90d762bf0
Mahliyo <gasandigital13@gmail.com>
2026-07-08 21:56:12 +0500

    mantiqiy operatorlar

--- main.js
+++ main.js
@@ -80,6 +80,6 @@
       // // let num = son.find((el)=> el >= 3);
       // console.log(num);
       const son = [20, 60, 40, 30,70];
-      let num = son.some();
+      let num = son.some((el)=>el > 35);
       console.log(num);
       

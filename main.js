2761121cad1fc65add7a1c253c9470b1927963b2
Mahliyo <gasandigital13@gmail.com>
2026-07-26 17:01:14 +0500

    array masalalari funksiyalarda

--- main.js
+++ main.js
@@ -422,7 +422,7 @@
 // ;
 
 function newArray(arr){
-  return[...new Set(arr)].sort;
+  return[...new Set(arr)].sort((a,b)=> a-b);
 }
 console.log(newArray([1,3,4,4,5,7,7,9,7,2,1]));
 console.log(newArray.sort);

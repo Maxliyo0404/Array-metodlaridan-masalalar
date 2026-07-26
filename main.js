a061eb2402a297bb769fc47acb7eb8e52032e5d3
Mahliyo <gasandigital13@gmail.com>
2026-07-26 17:48:47 +0500

    amasalalar to'plami filter

--- main.js
+++ main.js
@@ -446,3 +446,4 @@
 function harflar(harf){
   return harf.filter((item, index)=> harf.indexOf(item)=== index).sort();
 }
+console.log(harflar(["a", "s", "d", "k", "b", "h", "m", "q", "a", "b", "s", "h","b","a"]));

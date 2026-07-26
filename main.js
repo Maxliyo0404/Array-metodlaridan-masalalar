c880997d2b2de0658fe7ea05582ed14a28d2ca30
Mahliyo <gasandigital13@gmail.com>
2026-07-26 17:40:19 +0500

    amasalalar to'plami

--- main.js
+++ main.js
@@ -439,5 +439,5 @@
 // console.log(mevalar(["banan", "olma", "banan", "nok","olma", "gilos", "nok", "orik"]));
    
 function newArray(arr){
-  return arr.filter((item, index)=> arr.item ===index)
+  return arr.filter((item, index)=> arr.indexOf(item) ===index)
 }
